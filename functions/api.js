83f118367b672862f4304ce290f8c89d6181d1bf
Ravin <ravinwebsurgeon@gmail.com>
2023-04-04 18:18:14 +0530

    fixing api issue

--- functions/api.js
+++ functions/api.js
@@ -100,6 +100,48 @@ router.post("/add-contacts", async (req, res) => {
     }
 })
 
+
+
+router.post("/login"), async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user)
+        return res.json({ msg: "Username not found", status: false });
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid)
+        return res.json({ msg: "Incorrect Username or Password", status: false });
+      delete user.password;
+      return res.json({ status: true, user });
+    } catch (ex) {
+      next(ex);
+    }
+  };
+
+router.post("/register"), async (req, res, next) => {
+    try {
+      const { username, email, password, mobileNumber, role } = req.body;
+      const usernameCheck = await User.findOne({ email });
+      // if (usernameCheck)
+      //   return res.json({ msg: "Username already used", status: false });
+      const emailCheck = await User.findOne({ email });
+      if (emailCheck)
+        return res.json({ msg: "Email already used", status: false });
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await User.create({
+        email,
+        username,
+        password: hashedPassword,
+        mobileNumber,
+        role
+      });
+      delete user.password;
+      return res.json({ status: true, user });
+    } catch (ex) {
+      next(ex);
+    }
+  };
+
 router.post("/user-contacts", (req, res) => {
     const { email } = req.body;
 
